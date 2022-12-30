a44545b563bff27fbce10df6b2049cd81e0a86f7
MuchDevSuchCode <null@ransomware.run>
2022-12-30 16:10:24 -0600

    Initial Commit

---
+++ settings.py
@@ -0,0 +1,10 @@
+#PBKDF-HMAC Settings
+KEY_ALGORITHM = "SHA256"  # Encryption Algorithm
+KEY_LENGTH = 128          # Encryption Key Length
+KEY_SALT = "ABCDE12345"   # Enter a Static Salt
+KEY_ITERATIONS=500000     # Number of PBKDF Iterations
+
+# App SEttings
+
+KEYFILE_NAME = "key"      # By default the keyfile is named "key". Change the name here to anything you like.
+KEY_EXPIRATION = 120      # Defines how long to leave the keyfile on the hard disk before deleting it.
