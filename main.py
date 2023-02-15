a9345287255502f111cc4f3ece4b3b7aea4c03c2
Bill Dorman <nexthoprouter@gmail.com>
2023-02-15 03:53:41 +0000

    Removing OS

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 import hashlib
-import os
 import time
 import getpass
 import settings
+from flask import Flask
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-
-if os.path.exists("key"):
-  print(f"Deleting pre-existing key file...")
-  os.remove("key")
-else:
-  print(f"No key file found, continuing...")
 
 # Create salt
 salt = settings.KEY_SALT.encode()  # If you prefer to manually enter your salt and passphrase, comment this line and uncomment the two lines below.
@@ -41,11 +35,3 @@ aes512_hash = hashlib.sha512(key).hexdigest()
 # Print results
 # print("Salt: " + salt.hex())
 print("Hash: " + aes512_hash)
-
-f = open("key", "a")
-f.write(f'{key}')
-f.close()
-
-time.sleep(settings.KEY_EXPIRATION)
-print(f'Removing key file...')
-os.remove("key")
