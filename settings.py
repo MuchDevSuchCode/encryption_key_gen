b127b6e48461c6b8b83ea33f56311834b643d30b
MuchDevSuchCode <null@ransomware.run>
2023-02-16 18:20:36 -0600

    adding list of available hashing algorithms.

--- settings.py
+++ settings.py
@@ -3,3 +3,25 @@ KEY_ALGORITHM = "SHA256"
 KEY_LENGTH = 256
 KEY_SALT = "8d5e523ba6e23c7a9a295804dd78b8e31946c0c2d31e7eefcaceef5a57868193913050f35fb35ceafeb5d2e0fda4a40577809e928fd42f5c2434da38c0dd70f7690418d30bb6c4785a8dd"  # Change before production use
 KEY_ITERATIONS=500000
+
+
+
+
+# List of support hashing algorithms:
+
+# --SHA2 FAMILY--
+# SHA256        256-bit message digest
+# SHA512        512-bit messsage digest
+# SHA512_224    224-bit message digest
+# SHA512_256    256-bit message digest
+
+# BLAKE2        1-64-bit message digest
+
+# --SHA3 Family--
+# SHA3_224      224-bit message digest
+# SHA3_256      256-bit message digest
+# SHA3_384      384-bit message digest
+# SHA3_512      512-bit message digest
+# SHAKE128      > 128-bit message digest
+
+# SM3           256-bit message digest (used by Chinese govt.)
